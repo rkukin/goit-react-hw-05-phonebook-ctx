0f3769e9f9f6d1aa42f7f509436bbf026d15a7c4
rkukin <romakukin@gmail.com>
2020-03-05 08:21:02 +0200

    Updates

---
+++ precache-manifest.de6381c83aac02011e782f9ea7d6edd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811a53e08ddc9a6d42b9e48ddf1fc856",
+    "url": "/goit-react-hw-05-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "bc8ab36284cd201bb8ac",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/2.ca025b70.chunk.css"
+  },
+  {
+    "revision": "bc8ab36284cd201bb8ac",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.1cf446d3.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.1cf446d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2297fb150494c6057b",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.a2a1bc25.chunk.js"
+  },
+  {
+    "revision": "471224084cf842a1de0a",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/runtime-main.0181941c.js"
+  }
+]);
